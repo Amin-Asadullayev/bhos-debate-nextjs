7f3d56b93ea33639bf2d95a0425a8dc496db675b
Amin-Asadullayev <aminvit693@gmail.com>
2025-08-03 23:41:40 +0400

    SSG

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,56 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, get } from "firebase/database";
+import createDOMPurify from "isomorphic-dompurify";
+import { JSDOM } from "jsdom";
+import NoPage from "@/app/not-found"; 
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar"
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+const window = new JSDOM("").window;
+const DOMPurify = createDOMPurify(window);
+
+export async function generateStaticParams() {
+  const res = await get(ref(database, "blogs"));
+  const blogs = res.val();
+
+  if (!blogs) return [];
+
+  return Object.keys(blogs).map((id) => ({
+    id,
+  }));
+}
+
+export default async function BlogPost({ params }) {
+  const param = await params
+  const res = await get(ref(database, `blogs/${param.id}`));
+  const blog = res.val();
+  console.log(blog)
+
+  if (!blog) {
+    return <NoPage />;
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="blog-post">
+        <h1>{blog.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(blog.content) }} />
+      </div>
+      <Footer />
+    </>
+  );
+}
