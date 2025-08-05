d1060f30f6bad4748c8bab6a162d3151239b29af
Amin-Asadullayev <aminvit693@gmail.com>
2025-08-05 15:35:05 +0400

    Blog fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic"
 import Navbar from '@/components/Navbar'
 import Header from '@/components/Header'
 import About from '@/components/About'
@@ -5,14 +6,33 @@ import Events from '@/components/Events'
 import BlogSection from '@/components/BlogSection'
 import JoinUs from '@/components/JoinUs'
 import Contact from '@/components/Contact'
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, get } from "firebase/database";
 
-export default function Home(){
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+
+export default async function Home(){
+    const initialFetch = await get(ref(database, "blogs"))
+    const blogs = initialFetch.val() || {}
     return (<>
         <Navbar/>
         <Header/>
         <About/>
         <Events/>
-        <BlogSection/>
+        <BlogSection blogEntries={blogs}/>
         <JoinUs/>
         <Contact/>
         </>)
--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,24 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-
-export default async function BlogSection() {
-  const snap = await get(ref(database, "blogs"))
-  const blogEntries = snap.val() || {}
+export default async function BlogSection({blogEntries}) {
   return (
     <section id="blog" className="py-16 bg-gray-50 dark:bg-gray-700">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
