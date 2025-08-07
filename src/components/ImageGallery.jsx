e8d4f6eda5c0b2cac8c0d2287482a9fbf2254099
Amin-Asadullayev <aminvit693@gmail.com>
2025-08-07 17:43:06 +0400

    Fixing gallery navigation

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { NodeViewWrapper } from '@tiptap/react'
+import { FcPrevious, FcNext } from 'react-icons/fc'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
@@ -8,24 +9,43 @@ import { Pagination, Navigation } from 'swiper/modules'
 
 export const ImageGallery = ({ node }) => {
   const images = node.attrs.images || []
-  console.log('Node attributes:', node.attrs) 
+
+  const galleryPrevRef = useRef(null)
+  const galleryNextRef = useRef(null)
 
   return (
-    <NodeViewWrapper className="image-swiper-wrapper">
-    <Swiper
-      modules={[Pagination, Navigation]}
-      pagination={{ type: 'fraction' }}
-      spaceBetween={10}
-      navigation={true}
-      centeredSlides={true}
-      slidesPerView={1}
-    >
-      {images.map((src, id_) => (
-        <SwiperSlide key={id_}>
-          <img src={src} alt={`slide-${id_}`} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <NodeViewWrapper className="image-swiper-wrapper" style={{ position: 'relative' }}>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        pagination={{ type: 'fraction' }}
+        spaceBetween={10}
+        centeredSlides={true}
+        slidesPerView={1}
+        onSwiper={(swiper) => {
+          setTimeout(() => {
+            if (swiper.params.navigation) {
+              swiper.params.navigation.prevEl = galleryPrevRef.current
+              swiper.params.navigation.nextEl = galleryNextRef.current
+              swiper.navigation.destroy()
+              swiper.navigation.init()
+              swiper.navigation.update()
+            }
+          }, 0)
+        }}
+      >
+        {images.map((src, id_) => (
+          <SwiperSlide key={id_}>
+            <img src={src} alt={`slide-${id_}`} />
+          </SwiperSlide>
+        ))}
+
+        <div ref={galleryPrevRef} className="gallery-prev text-4xl">
+          <FcPrevious />
+        </div>
+        <div ref={galleryNextRef} className="gallery-next text-4xl">
+          <FcNext />
+        </div>
+      </Swiper>
     </NodeViewWrapper>
   )
 }
