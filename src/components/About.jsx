b593002c88ac5f80288be10982c9a87ddddf77f8
Tefma <aminvit693@gmail.com>
2025-08-09 00:13:02 +0400

    Fixing typo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
     return (
         <section id="about"  className="py-16 bg-gray-50 dark:bg-gray-900 transition-all duration-500 ease-in-out relative group">
-            <div className="absolute inset-0 bg-center bg-cover opacity-0 group-hover:opacity-50 transition-opacity duration-500" style={{ backgroundImage: "url('/banem.jpg')" }} ></div>
+            <div className="absolute inset-0 bg-center bg-cover opacity-0 group-hover:opacity-50 transition-opacity duration-500" style={{ backgroundImage: "url('/banem.JPG')" }} ></div>
             <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <h2 className="text-3xl font-bold text-gray-900 dark:text-white text-center mb-8">
                     About Our Club
