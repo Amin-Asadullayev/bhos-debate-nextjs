0397ac94e4b13411e2aaba59c3a5e2aa6a6e5345
VuqarAhadli <vuqarahadli17@gmail.com>
2025-08-08 23:54:23 +0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,19 @@
 const About = () => {
     return (
-        <section id="about" className="py-16 bg-gray-50 dark:bg-gray-900">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <h2 className="text-3xl font-bold text-gray-900 dark:text-white text-center mb-8">About Our Club</h2>
+        <section id="about"  className="py-16 bg-gray-50 dark:bg-gray-900 transition-all duration-500 ease-in-out relative group">
+            <div className="absolute inset-0 bg-center bg-cover opacity-0 group-hover:opacity-50 transition-opacity duration-500" style={{ backgroundImage: "url('/banem.jpg')" }} ></div>
+            <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h2 className="text-3xl font-bold text-gray-900 dark:text-white text-center mb-8">
+                    About Our Club
+                </h2>
                 <p className="text-lg text-gray-600 dark:text-gray-300 text-center max-w-3xl mx-auto">
-                    The Debate Club is a vibrant community dedicated to fostering intellectual growth, critical thinking, and effective communication. We welcome individuals from all backgrounds to engage in respectful and dynamic discussions on a wide range of topics.
+                    The Debate Club is a vibrant community dedicated to fostering intellectual growth,
+                    critical thinking, and effective communication. We welcome individuals from all
+                    backgrounds to engage in respectful and dynamic discussions on a wide range of topics.
                 </p>
             </div>
         </section>
-    )
-}
+    );
+};
+
 export default About;
