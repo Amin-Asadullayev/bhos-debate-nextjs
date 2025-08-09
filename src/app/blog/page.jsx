24e4e0185e9a4d86893a037b208164fabbdddd2a
VuqarAhadli <vuqarahadli17@gmail.com>
2025-08-09 17:32:14 +0400

    Update page.jsx

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -32,7 +32,7 @@ export default async function BlogList() {
     <>
       <Navbar />
       <div className="max-w-4xl mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-6">All Blog Posts</h1>
+        <h1 className="text-3xl font-bold mb-6">Blog</h1>
 
         {!blogs && <p>No blogs found.</p>}
 
