df5039aad767388eec57b2af35399a1055ca7874
Amin-Asadullayev <aminvit693@gmail.com>
2025-08-13 22:29:31 +0400

    Penultimate version

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -2,20 +2,20 @@ const JoinUs = () => {
     return (
         <section id="join" className="py-16 bg-gray-50 dark:bg-gray-900 relative group transition-all duration-500 ease-in-out" >
 
-            <div className="absolute inset-0 bg-center bg-cover opacity-0 group-hover:opacity-50 md:opacity-0 md:group-hover:opacity-40 transition-opacity duration-500" style={{ backgroundImage: "url('/felsefe.jpg')" }} ></div>
+            <div className="absolute inset-0 bg-center bg-cover opacity-60 md:opacity-60 transition-opacity duration-500" style={{ backgroundImage: "url('/felsefe.jpg')" }} ></div>
 
 
             <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
                 <h2 className="text-3xl font-bold text-gray-900 dark:text-white mb-8">
                     Join Our Club
                 </h2>
-                <p className="text-lg text-gray-600 dark:text-gray-300 mb-8 max-w-2xl mx-auto">
+                <p className="text-lg text-gray-800 dark:text-gray-300 mb-8 max-w-2xl mx-auto">
                     Become a part of our community! Whether you're a seasoned debater or just starting out,
                     we have a place for you.
                 </p>
                 <a
                     href="#contact"
-                    className="nav-link inline-block bg-primary text-white font-semibold py-3 px-6 rounded-lg hover:bg-primary-200 transition"
+                    className="nav-link inline-block bg-gray-600 dark:bg-primary text-white font-semibold py-3 px-6 rounded-lg hover:bg-gray-400 transition"
                 >
                     Sign Up Now
                 </a>
--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -1,7 +1,8 @@
 const News = ({ news }) => {
     return (
-        <section id="events" className="py-16 bg-white dark:bg-gray-800">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <section id="about"  className="py-16 bg-gray-300 dark:bg-gray-800 transition-all duration-500 ease-in-out relative group">
+        <div className="absolute inset-0 bg-center bg-cover opacity-0 group-hover:opacity-50 transition-opacity duration-500" style={{ backgroundImage: "url('/banem.JPG')" }} ></div>
+        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <h2 className="text-3xl font-bold text-gray-900 dark:text-white text-center mb-8">News</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {
@@ -11,7 +12,7 @@ const News = ({ news }) => {
                                     <h3 className="text-xl font-semibold text-gray-900 dark:text-white">{item.title}</h3>
                                     <p className="text-gray-600 dark:text-gray-300">{["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][(new Date(-item.date)).getMonth()] + " " + (new Date(-item.date)).getDate() + ", " + (new Date(-item.date)).getFullYear()}</p>
 
-                                    <span className="inline-block mt-4 text-primary font-semibold">
+                                    <span className="inline-block mt-4 text-gray-800 dark:text-gray-400 font-semibold">
                                         Read more →
                                     </span>
                                 </div>
