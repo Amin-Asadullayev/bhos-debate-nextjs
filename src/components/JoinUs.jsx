cea52f4f9f05cc60683b6b42118613e0ecfffa2b
domennazirliyi <domennazirliyi@gmail.com>
2025-08-24 02:55:18 +0400

    Update JoinUs.jsx

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -2,7 +2,8 @@ const JoinUs = () => {
     return (
         <section id="join" className="py-16 bg-gray-50 dark:bg-gray-900 relative group transition-all duration-500 ease-in-out">
 
-            <div className="absolute inset-0 bg-center bg-coveropacity-60 md:opacity-0 md:group-hover:opacity-60transition-opacity duration-500"style={{ backgroundImage: "url('/felsefe.jpg')" }}></div>
+            <div className="absolute inset-0 bg-center bg-cover opacity-60 md:opacity-0 md:group-hover:opacity-60 transition-opacity duration-500"  style={{ backgroundImage: "url('/felsefe.jpg')" }} ></div>
+
             <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
                 <h2 className="text-3xl font-bold text-gray-900 dark:text-white mb-8">
                     Join Our Club
@@ -11,7 +12,10 @@ const JoinUs = () => {
                     Become a part of our community! Whether you're a seasoned debater or just starting out,
                     we have a place for you.
                 </p>
-                <ahref="#contact"className="nav-link inline-block bg-gray-600 dark:bg-primary text-white font-semibold py-3 px-6 rounded-lg hover:bg-gray-400 transition">
+                <a
+                    href="#contact"
+                    className="nav-link inline-block bg-gray-600 dark:bg-primary text-white font-semibold py-3 px-6 rounded-lg hover:bg-gray-400 transition"
+                >
                     Sign Up Now
                 </a>
             </div>
