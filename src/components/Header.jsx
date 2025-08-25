6aef2c23c96c1bd8444f309edef55ccb770ca2ae
Amin-Asadullayev <aminvit693@gmail.com>
2025-08-25 15:46:18 +0400

    Animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,25 @@
+"use client"
+import { useEffect, useState } from "react"
 const Header = () => {
+  const spans = ["Philosophy", "Communication", "Confidence", "Debate"]
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prev => (prev+1)%4)
+    }, 3000)
+    return () => clearInterval(() => {})
+  }, [])
+  
   return (
     <section className="relative bg-primary text-white py-20 overflow-hidden group">
       <div className="absolute inset-0 bg-center bg-cover opacity-40" style={{ backgroundImage: "url('/fel.jpg')" }}   ></div>
 
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h2 className="text-4xl md:text-5xl font-extrabold mb-4 text-center">
-          <span className="block md:hidden">Join the Art of Debate</span>
+          
 
-          <span className="hidden md:inline-block">
-            Join the Art of{" "}
-            <span className="dropping-texts">
-              <div>Philosophy</div>
-              <div>Communication</div>
-              <div>Confidence</div>
-              <div>Debate</div>
-            </span>
+          <span className="hidden inline-block">
+            Join the Art of <span key={count} className="transition-opacity duration-500 ease-in-out opacity-0 animate-fadeIn">{spans[count]}</span>
           </span>
         </h2>
 
