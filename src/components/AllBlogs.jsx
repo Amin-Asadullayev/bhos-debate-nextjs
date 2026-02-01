a4d4e2ecfeb6b8f345de2b326866b3e78c89c76d
toghrulabdullayev <toghrulabdullayev1@gmail.com>
2026-02-02 00:40:24 +0400

    fix news and blog id bugs

--- src/components/AllBlogs.jsx
+++ src/components/AllBlogs.jsx
@@ -1,10 +1,15 @@
 import BlogCard from "./BlogCard";
 
 function AllBlogs({ blogs }) {
+  // Handle both array and object formats
+  const blogsArray = Array.isArray(blogs)
+    ? blogs
+    : Object.entries(blogs).map(([id, item]) => ({ ...item, id }));
+
   return (
     <div className="grid md:grid-cols-3 gap-8 mb-8">
-      {Object.entries(blogs).map(([id, item], index) => (
-        <BlogCard key={id} blog={{ ...item, id }} index={index} />
+      {blogsArray.map((blog, index) => (
+        <BlogCard key={blog.id} blog={blog} index={index} />
       ))}
     </div>
   );
--- src/components/AllNews.jsx
+++ src/components/AllNews.jsx
@@ -1,10 +1,15 @@
 import NewsCard from "./NewsCard";
 
 function AllNews({ news }) {
+  // Handle both array and object formats
+  const newsArray = Array.isArray(news)
+    ? news
+    : Object.entries(news).map(([id, item]) => ({ ...item, id }));
+
   return (
     <div className="space-y-6 mb-8">
-      {Object.entries(news).map(([id, item], index) => (
-        <NewsCard key={id} news={{ ...item, id }} index={id} />
+      {newsArray.map((newsItem, index) => (
+        <NewsCard key={newsItem.id} news={newsItem} index={index} />
       ))}
     </div>
   );
